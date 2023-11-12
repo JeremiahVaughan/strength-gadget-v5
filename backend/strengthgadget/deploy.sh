86f9a7f6a6605642e08606ed5b8174cc793c6323
jeremiahvaughan <jeremiah.t.vaughan@gmail.com>
2023-11-12 11:39:44 -0500

    t

--- backend/strengthgadget/deploy.sh
+++ backend/strengthgadget/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USER" --password-stdin
+echo "$TF_VAR_docker_token" | docker login -u "$TF_VAR_docker_user" --password-stdin
 docker buildx create --use
-docker buildx build --platform linux/arm64 --push -t "$DOCKER_USER/strengthgadget:$CIRCLE_WORKFLOW_ID" .
+docker buildx build --platform linux/arm64 --push -t "$TF_VAR_docker_user/strengthgadget:$TF_VAR_circle_workflow_id" .
