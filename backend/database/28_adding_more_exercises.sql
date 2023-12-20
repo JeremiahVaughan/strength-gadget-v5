7090fdbedf15c75ad95fde27c1e97684b01228a6
Jeremiah Vaughan <46267375+JeremiahVaughan@users.noreply.github.com>
2023-12-19 21:03:07 -0600

    adding more exercises.

---
+++ backend/database/28_adding_more_exercises.sql
@@ -0,0 +1,20 @@
+INSERT INTO exercise (id, name, demonstration_giphy_id, exercise_type_id)
+VALUES ('b84acd17-670d-4449-a249-4bba40f00c8c', 'Reverse Lunges', 'JO4nBEsc6Dlyw3FAvX', '6bdb3624-bed1-41a9-bf8c-7b1066411446');
+INSERT INTO exercise_muscle_group (exercise_id, muscle_group_id)
+VALUES ('b84acd17-670d-4449-a249-4bba40f00c8c', '305a9027-d72c-4e60-9ab3-c07d854f76c5');
+INSERT INTO exercise_muscle_group (exercise_id, muscle_group_id)
+VALUES ('b84acd17-670d-4449-a249-4bba40f00c8c', '38f352d6-f290-443a-84f7-baf38f54b5b2');
+
+INSERT INTO exercise (id, name, demonstration_giphy_id, exercise_type_id)
+VALUES ('18243ee5-099e-406b-8344-9dce2b9ca023', 'Back Squats', 'W5gFEeJmRhvElyatmF', '6bdb3624-bed1-41a9-bf8c-7b1066411446');
+INSERT INTO exercise_muscle_group (exercise_id, muscle_group_id)
+VALUES ('18243ee5-099e-406b-8344-9dce2b9ca023', '305a9027-d72c-4e60-9ab3-c07d854f76c5');
+INSERT INTO exercise_muscle_group (exercise_id, muscle_group_id)
+VALUES ('18243ee5-099e-406b-8344-9dce2b9ca023', '38f352d6-f290-443a-84f7-baf38f54b5b2');
+
+INSERT INTO exercise (id, name, demonstration_giphy_id, exercise_type_id)
+VALUES ('466ba3dd-b1aa-4b79-8cce-934f4cd08e07', 'Front Squats', 'AmYRXqZyVy0GVigC2l', '6bdb3624-bed1-41a9-bf8c-7b1066411446');
+INSERT INTO exercise_muscle_group (exercise_id, muscle_group_id)
+VALUES ('466ba3dd-b1aa-4b79-8cce-934f4cd08e07', '38f352d6-f290-443a-84f7-baf38f54b5b2');
+
+
