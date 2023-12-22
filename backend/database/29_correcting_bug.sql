0bce6548a237d73cf1c1a319803b8334d606b522
Jeremiah Vaughan <46267375+JeremiahVaughan@users.noreply.github.com>
2023-12-22 17:20:49 -0600

    Forgot to add measurement type to 2 exercises.

---
+++ backend/database/29_correcting_bug.sql
@@ -0,0 +1,8 @@
+UPDATE public.exercise
+SET measurement_type_id = '0c7a1eb7-1b61-4bcd-b136-8778e99e5b06'
+WHERE id = '466ba3dd-b1aa-4b79-8cce-934f4cd08e07'::uuid;
+
+UPDATE public.exercise
+SET measurement_type_id = '0c7a1eb7-1b61-4bcd-b136-8778e99e5b06'
+WHERE id = '18243ee5-099e-406b-8344-9dce2b9ca023'::uuid;
+
