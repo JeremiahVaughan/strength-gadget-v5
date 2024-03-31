42ab231bfb0d2ec78fb8f857b4a3209a325ac621
Jeremiah Vaughan <46267375+JeremiahVaughan@users.noreply.github.com>
2024-03-31 14:27:30 -0500

    adding nx build token.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sed -i "s/\"VERSION_PLACEHOLDER\"/\"$CIRCLE_WORKFLOW_ID\"/" ./public/health.json
+echo "NX_CLOUD_ACCESS_TOKEN=$TF_VAR_nx_cloud_access_token" > nx-cloud.env
 npm i
 npx nx build --configuration=production
