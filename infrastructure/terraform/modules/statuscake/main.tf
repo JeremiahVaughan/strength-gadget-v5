b412469a4b29f6fa83a8ea9ed04a2ce16319d0dd
Jeremiah Vaughan <46267375+JeremiahVaughan@users.noreply.github.com>
2024-06-30 18:14:12 -0500

    correcting the Test name

--- infrastructure/terraform/modules/statuscake/main.tf
+++ infrastructure/terraform/modules/statuscake/main.tf
@@ -1,6 +1,6 @@
 module "status_cake_server" {
   source = "./endpoint"
-  name = "Server Health"
+  name = "${var.domain_name} Server Health"
   endpoint = "https://${var.domain_name}/health"
   status_cake_contact_group_id = var.status_cake_contact_group_id
   circle_workflow_id = var.circle_workflow_id
